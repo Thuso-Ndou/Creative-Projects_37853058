137dfd43a16d0547968bb708c205068f1c3a827d
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2023-10-05 04:28:03 +0200

    A function for transferring funds is created

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@ add funds, remove funds, send funds to others, and check the balance of your wal
 */
 #include <iostream>
 #include <string>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -23,9 +25,9 @@ struct digitalWallet
 // Function prototype
 void createWallet(digitalWallet* walletArray, int &walletCount);
 void addFunds(digitalWallet *walletArray,string walletID, int pin, double amount, int walletCount);
-void withdrawFunds();
-void generateRandomReference();
-void transferFunds();
+void withdrawFunds(digitalWallet* walletArray, string walletID, int pin, double amount, int walletCount);
+int generateRandomReference();
+void transferFunds(digitalWallet* walletArray, string senderWallet, int senderPin,int walletCount, string recipientWallet, double amount);
 void viewBalance(digitalWallet* walletArray,string accountId, int pin, int walletCount);
 
 int main()
@@ -36,7 +38,6 @@ int main()
     int walletCount = 0;
     digitalWallet newWallet[2];
     digitalWallet* pNewWallet = newWallet;
-    double amount;
 
     // Display welcome Message
     printf("%46s","Welcome To The Digital Wallet System\n");
@@ -70,8 +71,10 @@ int main()
         // convert a string input an integer
         userChoice = stoi(sUserChoice);
 
-        // declare variables
+        // declare variables to use inside the switch
         string accountID;
+        string recipientID;
+        double amount;
         int pin;
 
         switch(userChoice)
@@ -97,10 +100,43 @@ int main()
                 addFunds(pNewWallet, accountID, pin, amount, walletCount);
                 break;
             case 3:
-                withdrawFunds();
+                // user input for account id
+                cout << "\nEnter Account ID: ";
+                cin.ignore();
+                cin >> accountID;
+
+                // user input for wallet pin
+                cout << "Enter Wallet Pin: ";
+                cin >> pin;
+
+                // user input for amount to fund
+                cout << "Enter Withdrawal Amount: R ";
+                cin >> amount;
+
+                // function call for withdrawing funds
+                withdrawFunds(pNewWallet, accountID, pin, amount, walletCount);
                 break;
             case 4:
-                transferFunds();
+                // user input for account id
+                cout << "\nEnter Account ID: ";
+                cin.ignore();
+                cin >> accountID;
+
+                // user input for wallet pin
+                cout << "Enter Wallet Pin: ";
+                cin >> pin;
+
+                // user input for recipient wallet id
+                cout << "Enter Recipient Account: ";
+                cin.ignore();
+                cin >> recipientID;
+
+                // user input for amount to fund
+                cout << "Enter Transfer Amount: R ";
+                cin >> amount;
+
+                // call function for transfering funds
+                transferFunds(pNewWallet, accountID, pin, walletCount, recipientID, amount);
                 break;
             case 5:
                 // user input for account id
@@ -219,18 +255,158 @@ void addFunds(digitalWallet *walletArray,string walletID, int pin, double amount
 }
 
 // function for withdrawal of funds from wallet
-void withdrawFunds()
+void withdrawFunds(digitalWallet* walletArray, string walletID, int pin, double amount, int walletCount)
 {
+    // when the is no wallet
+    if(walletCount == 0)
+    {
+        cout << "\nWallet Not Found.\n";
+    }
+    else
+    {
+        // input validation for fund amount
+        if(amount < 0)
+        {
+            do
+            {
+                // error message for amount less than zero
+                cout << "Error! Enter a Positive Amount.\n";
+
+                // input for amount
+                cout << "Enter Amount: R ";
+                cin >> amount;
+
+            } while (amount < 0);
+        }
+        // search for the wallet using a loop
+        for(int i = 0; i < walletCount; i++)
+        {
+            if(walletArray[i].walletId == walletID)
+            {
+                if(walletArray[i].balance >= amount)
+                {
+                    // declare withdrawal reference variable
+                    int referenceNumber;
+
+                    // assign a random reference number
+                    referenceNumber = generateRandomReference();
+
+                    // incremet amount
+                    walletArray[i].balance -= amount;
+
+                    // display a success message
+                    cout << "\nWithdrawal successfully. New balance: R " << walletArray[i].balance << "\n";
+
+                    // display the reference number
+                    cout << "\nWithdrawal reference number: " << referenceNumber << endl;
+
+                    // break outside the loop
+                break;
+                }
+                else
+                {
+                    //error message fr invalid amount
+                    cout << "\nInsufficient Funds.\n";
+                }
+            }
+            else if(walletArray[i].walletId == walletID && walletArray[i].pin != pin)
+            {
+                do
+                {
+                    // display a error message for incorrect pin
+                    cout << "\nIncorrect Pin. Access denied.\n";
+
+                    // user input for pin
+                    cout << "Enter Pin: ";
+                    cin >> pin;
+
+                } while(pin != walletArray[i].pin);
+
+
+                // break outside the loop
+                break;
+            }
+            else
+            {
+                cout << "\nWallet Not Found.\n";
+            }
+        }
+    }
 }
 
 // function for generating random numbers for withdrawal reference
-void generateRandomReference()
+int generateRandomReference()
 {
+    // declare a random number variable
+    int randomNumber;
+
+    // generate a random number
+    randomNumber = 1000000000 + rand() % 9999999999;
+
+    // return the generated number
+    return randomNumber;
 }
 
 // Function to transfer funds to another wallet
-void transferFunds()
+void transferFunds(digitalWallet* walletArray, string senderWallet, int senderPin, int walletCount,string recipientWallet, double amount)
 {
+    // search for the wallet using a loop
+        for(int i = 0; i < walletCount; i++)
+        {
+            if(walletArray[i].walletId == senderWallet && walletArray[i].pin == senderPin)
+            {
+                if(walletArray[i].pin != senderPin)
+                {
+                    do
+                    {
+                        // display a error message for incorrect pin
+                        cout << "\nIncorrect Pin. Access denied.\n";
+
+                        // user input for pin
+                        cout << "Enter Pin: ";
+                        cin >> senderPin;
+
+                    } while(senderPin != walletArray[i].pin);
+                }
+
+                //search for recipient wallet id
+                for(int j = 0; j < walletCount; j++)
+                {
+                    if(recipientWallet == walletArray[j].walletId)
+                    {
+                        if(walletArray[i].balance >= amount)
+                        {
+                            // decrement sender wallet balance by amount
+                            walletArray[i].balance -= amount;
+
+                            // increment recipient balance by 1
+                            walletArray[j].balance += amount;
+
+                            // display success message
+                            cout << "Transfer Successful. New Balance: R" << walletArray[i].balance << endl;
+
+                            //break of the loop
+                            break;
+                        }
+                        else
+                        {
+                            // display an insufficient funds message
+                            cout << "\nInsufficient Funds.\n";
+                        }
+                    }
+                    else
+                    {
+                        // display message showing recipient not found
+                        cout << "\nRecipient Wallet Not Found.\n";
+                    }
+                }
+            }
+
+            else
+            {
+                cout << "\nWallet Not Found.\n";
+            }
+        }
 }
 
 // Function to display wallet balance
